d69e6bb1e90011712a82ed9d353df1be09d01a4a
unknown <ivan_solenkov1999@abv.bg>
2017-10-27 15:45:11 +0300

    New items added

---
+++ Files and Exceptions - Exercises/Problem06.FixEmails/FixEmails.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Problem06.FixEmails
+{
+    class FixEmails
+    {
+        static void Main()
+        {
+            var lines = File.ReadAllLines("input.txt");
+            File.Delete("output.txt");
+            for (int i = 0; i < lines.Length; i+=2)
+            {
+                if (lines[i] == "stop" || lines[i + 1] == "stop")
+                {
+                    break;
+                }
+
+                var name = lines[i];
+                var emails = lines[i + 1];
+                if (emails.EndsWith(".uk") || emails.EndsWith(".us"))
+                {
+                    continue;
+                }
+
+                var output = $"{name} -> {emails}" + Environment.NewLine;
+
+                File.AppendAllText("output.txt", output);
+            }
+        }
+    }
+}
