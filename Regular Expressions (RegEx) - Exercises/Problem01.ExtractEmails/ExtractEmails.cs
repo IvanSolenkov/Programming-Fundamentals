7dc6096b31018ba9b8c6e48022bd6ea0d2f1d109
unknown <ivan_solenkov1999@abv.bg>
2017-10-31 14:39:59 +0200

    New project added

---
+++ Regular Expressions (RegEx) - Exercises/Problem01.ExtractEmails/ExtractEmails.cs
@@ -0,0 +1,22 @@
+﻿namespace _01.ExtractEmails
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    public class ExtractEmails
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+            var pattern = @"(?<=\s)([a-z]+|\d+)(\d+|\w+|\.+|-+)([a-z]+|\d+)\@[a-z]+\-?[a-z]+\.[a-z]+(\.[a-z]+)?";
+            Regex regex = new Regex(pattern);
+            var matches = Regex.Matches(input, pattern);
+            foreach (Match match in matches)
+            {
+                Console.WriteLine(match);
+            }
+        }
+    }
+}
